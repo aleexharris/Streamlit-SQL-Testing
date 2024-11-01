e38d7b09145d4a0837299ef090c5a2f564878810
Alex On Linux PC <alex@harris-co.com>
2024-11-01 22:50:27 +0000

    fix: updated contsts

--- consts.py
+++ consts.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 LIVE = os.getenv("LIVE", "false").lower() == "true"
-QUERY_CACHE_LEN = 15
+QUERY_CACHE_LEN = 10
 QUERY_PREVIEW_LEN = 100
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
 MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
